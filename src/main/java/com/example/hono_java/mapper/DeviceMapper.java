bf0a9e4b3f561c8d57a761a6a4c44e58203f50e6
wantunderstandu <3264935651@qq.com>
2026-07-08 10:01:24 +0800

    精简后的项目

--- src/main/java/com/example/hono_java/mapper/DeviceMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.hono_java.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.hono_java.modal.dto.DeviceEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DeviceMapper extends BaseMapper<DeviceEntity> {
-}
